61511d8a5d0707968b99c4d1cf22056dc10c62e1
epsilonlyrae <kozlov.de@phystech.edu>
2022-11-29 03:57:07 +0300

    created game manager class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "field_class.hpp"
 #include <SFML/Graphics.hpp>
-
+#include <map>
+#include<string>
 void update_Image(fc :: Scalar_Field& scalar, sf :: Image& image) {
 
     for (int i = 0; i < fc ::  N; i++) {
@@ -19,33 +20,72 @@ void update_Image(fc :: Scalar_Field& scalar, sf :: Image& image) {
     }
 }
 
-const int FPS  = 30;
+
+class Game_manager {
+
+    public  :
+
+
+        Game_manager() {
+            texture.create(fc :: N, fc :: N);
+            sprite.setTexture(texture);
+            image.create(fc :: N, fc :: N, sf :: Color ::  Red);
+        }
+
+        const  static int FPS = 30;
+
+        const static int BAD_FPS = 27;
+
+        void pause() {
+            game_state["paused"] = !game_state["paused"];
+        }
+
+
+        void draw_my_scalar_field(sf :: RenderWindow& window) {
+            update_Image(scalar, image);
+            texture.update(image);
+            window.draw(sprite);
+        }
+
+        void evolve_my_scalar_field(double dt) {
+
+            if (!game_state["paused"]) {
+                scalar.evolve(dt);
+            }
+        }
+
+
+        void disturb_my_scalar_field(int x, int y, int width, int length, int amplitude) {
+            scalar.create_disturbance(x, y, width, length, amplitude);
+        }
+
+
+    private:
+
+        std :: map<std :: string, bool> game_state {{"paused", false}};
+
+
+        fc :: Scalar_Field scalar;
+
+        sf :: Image image;
+
+        sf :: Texture texture;
+
+        sf :: Sprite sprite;
+
+
+};
+
 
 int main() {
 
+    Game_manager manager;
 
-    fc :: Scalar_Field scalar;
-
-    scalar.create_disturbance((fc :: N) / 2 , (fc:: N) / 2,  10, 100, 5);
+    manager.disturb_my_scalar_field((fc :: N) / 2 , (fc:: N) / 2,  10, 100, 5);
 
     sf::RenderWindow window(sf::VideoMode(fc :: N, fc :: N), "SIV4");
+    window.setFramerateLimit(manager.FPS);
 
-    window.setFramerateLimit(FPS);
-
-
-    sf :: Image image;
-    image.create(fc :: N, fc :: N, sf :: Color ::  Red);
-
-    update_Image(scalar, image);
-
-
-
-    sf :: Texture texture;
-    texture.create(fc :: N, fc :: N);
-    texture.update(image);
-
-    sf :: Sprite sprite;
-    sprite.setTexture(texture);
 
     sf::Vertex line1 [] =
     {
@@ -81,41 +121,42 @@ int main() {
                 if (event.key.code == sf::Keyboard::Return) {
                     window.close();
                 }
+
+                if (event.key.code == sf::Keyboard:: Space) {
+                    manager.pause();
+                }
             }
 
 
             if (event.type = sf :: Event :: MouseButtonPressed) {
 
-                scalar.create_disturbance(event.mouseButton.x, event.mouseButton.y,  50, 50,  1);
+               manager.disturb_my_scalar_field(event.mouseButton.x, event.mouseButton.y,  50, 50,  1);
             }
         }
 
 
         window.clear(sf::Color::Black);
 
-        window.draw(sprite);
+
+
+        manager.evolve_my_scalar_field(1);
+        manager.draw_my_scalar_field(window);
 
         window.draw(line1, 2, sf :: Lines);
         window.draw(line2, 2, sf :: Lines);
 
-        update_Image(scalar, image);
-
-        texture.update(image);
-
         window.display();
-
-        scalar.evolve(1);
-
 
         currentTime = clock.getElapsedTime();
         float fps = 1.0f / (currentTime.asSeconds() - previousTime.asSeconds());
-        if (fps <= 27) {
+        if (fps <= manager.BAD_FPS) {
             slow_counter++;
         }
         previousTime = currentTime;
 
 
-    }
+}
+
     std :: cout <<  "slow_frames " << slow_counter << '\n';
     std :: cout << "time_elapsed " <<  floor(currentTime.asSeconds()) << '\n';
 
