4e9c7d1b06a08fb6758ef028c35ee159f916c396
knyazevmatvey <knyazevmatvey@yandex.ru>
2022-12-17 13:48:39 +0300

    Changed config from code to text (not working yet)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,60 @@
 #include <SFML/Graphics.hpp>
 #include <map>
-#include<string>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
 
 #include "simulation.hpp"
 
 
 using namespace sim;
+
+
+template<int size>
+void analyze_string(SimulationManager<size>& manager, std::string init_str) {
+    std::istringstream stream(init_str);
+    std::string type;
+    stream >> type;
+    type = init_str.substr(0, init_str.find(" "));
+    if ((type == "reflection_wall") || (type == "absorbtion_wall")) {
+        int first_coord, second_begin, second_end;
+        std::string position;
+        stream >> first_coord >> second_begin >> second_end >> position;
+
+        bool vertical = true;
+        if (position == "vertical") vertical = true;
+        if (position == "horizontal") vertical = false;
+
+        if (type == "reflection_wall") manager.add_reflecting_wall(first_coord, second_begin, second_end, vertical);
+        else manager.add_super_absorber(first_coord, second_begin, second_end, vertical);
+    }
+    if (type == "harmonic_source") {
+        int x, y;
+        double period, amplitude, phase;
+        stream >> x >> y >> period >> amplitude >> phase;
+        manager.add_harmonic_oscillator(x, y, period, amplitude, phase);
+    }
+    if (type == "impulse_source") {
+        int x, y, period;
+        double amplitude;
+        stream >> x >> y >> period >> amplitude;
+        manager.add_impulse_oscillator(x, y, period, amplitude);
+    }
+}
+
+template<int size>
+void analyze_input(SimulationManager<size>& manager) {
+    std::ifstream file("configurations/config.txt");
+    while (true) {
+        std::string temp;
+        std::getline(file, temp);
+        std::cout << "line: " << temp << std::endl;
+        if (temp == "") break;
+        analyze_string(manager, temp);
+    }
+    file.close();
+}
 
 int main() {
 
@@ -27,19 +76,21 @@ int main() {
 
     sim :: FPSdrawer fps_drawer(manager.get_size() / 15, 0, 0, font);
 
+    analyze_input(manager);
     // Space for configuration
-    {
-        manager.add_reflecting_wall(45, 55, 245, true);
-        manager.add_reflecting_wall(255, 55, 245, true);
-        manager.add_reflecting_wall(45, 55, 245, false);
-        manager.add_reflecting_wall(255, 55, 245, false);
+    /*{
+        manager.add_super_absorber(130, 0, 90, true);
+        manager.add_super_absorber(130, 100, 145, true);
+        manager.add_super_absorber(130, 155, 200, true);
+        manager.add_super_absorber(130, 210, 300, true);
+        manager.add_harmonic_oscillator(50, 150, 20, 4, 0);
         // manager.add_reflecting_wall(x, y_begin, y_end, true); -- vertical reflecting wall
         // manager.add_reflecting_wall(y, x_begin, x_end, true); -- horizontal reflecting wall
         // manager.add_super_absorber(x, y_begin, y_end, true); -- vertical absorbing wall
         // manager.add_super_absorber(y, x_begin, x_end, false); -- horizontal absorbing wall
         // manager.add_impulse_oscillator(x, y, period, ampl);
         // manager.add_harmonic_oscillator(x, y, period, ampl, phase);
-    }
+    }*/
 
     sf::RenderWindow window(sf::VideoMode(manager.get_size(), manager.get_size()), "SIV4");
     window.setFramerateLimit(manager.FPS);
